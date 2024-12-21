6cb2908be89e1ce4189d6d63f20558a86b2700d6
neko-no <101441574+neko-no@users.noreply.github.com>
2024-12-21 17:05:53 +0900

    #1 add: フォームの簡単な例を作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,14 @@
 "use client";
 
 import { FC } from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
-
-type Inputs = {
-  email: string;
-  password: string;
-};
+import { FoodDeliveryForm } from "./FoodDeliveryForm";
 
 const Page: FC = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<Inputs>();
-
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
-
   return (
-    <div className="App container my-8 bg-white">
-      <h1 className="text-center mb-8 text-lg">ログイン</h1>
-      <form onSubmit={handleSubmit(onSubmit)} className=" space-y-4">
-        <div>
-          <label htmlFor="email" className="mr-2">
-            Email
-          </label>
-          <input
-            id="email"
-            {...register("email", { required: "必須です" })}
-            className="border"
-          />
-          {errors.email && errors.email.message}
-        </div>
-        <div>
-          <label htmlFor="password" className="mr-2">
-            Password
-          </label>
-          <input
-            id="password"
-            {...register("password")}
-            type="password"
-            className="border"
-          />
-        </div>
-        <button className="border block mx-auto" type="submit">
-          ログイン
-        </button>
-      </form>
+    <div className="container">
+      <div className="mx-5">
+        <FoodDeliveryForm />
+      </div>
     </div>
   );
 };
