5c60610ce9158c2e888c95b6278b67f70b79dbba
neko-no <101441574+neko-no@users.noreply.github.com>
2024-12-21 21:00:42 +0900

    #7 add: TextFieldコンポーネントの作成

--- src/app/FoodDeliveryForm.tsx
+++ src/app/FoodDeliveryForm.tsx
@@ -1,7 +1,9 @@
 import { useForm, FieldErrors } from "react-hook-form";
+import TextField from "./controls/TextField";
 
 type FoodDeliveryFormType = {
   customerName: string;
+  customerField: string;
   mobile: string;
   orderNo: number;
   Email: string;
@@ -43,87 +45,77 @@ export const FoodDeliveryForm = () => {
     <form autoComplete="off" onSubmit={handleSubmit(onSubmit, onError)}>
       <div className="row mb-2">
         <div className="col">
-          <div className="form-floating">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="#Order No."
-              {...register("orderNo", {
-                required: "Customer name is required.",
-              })}
-            />
-            <label>#Order No.</label>
-          </div>
+          <TextField
+            label="#Order No."
+            {...register("orderNo", {
+              required: "orderNo is required.",
+            })}
+            error={errors.orderNo}
+          />
         </div>
         <div className="col">
-          <div className="form-floating">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Mobile"
-              {...register("mobile", {
-                minLength: {
-                  value: 10,
-                  message: "Must be 10 digits.",
-                },
-                maxLength: {
-                  value: 10,
-                  message: "Must be 10 digits.",
-                },
-                required: "This field is required.",
-              })}
-            />
-            <label>Mobile</label>
-            {errors.mobile && (
-              <div className="error-feedback">{errors.mobile?.message}</div>
-            )}
-          </div>
+          <TextField
+            label="Mobile"
+            {...register("mobile", {
+              minLength: {
+                value: 10,
+                message: "Must be 10 digits.",
+              },
+              maxLength: {
+                value: 10,
+                message: "Must be 10 digits.",
+              },
+              required: "This field is required.",
+            })}
+            error={errors.mobile}
+          />
+        </div>
+        <div className="col">
+          <TextField
+            label="Custom Field"
+            className="border-success"
+            {...register("customerField", {
+              required: "Customer name is required.",
+            })}
+            error={errors.customerField}
+          />
         </div>
       </div>
       <div className="row">
         <div className="col">
-          <div className="form-floating mb-3">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Customer Name"
+          <div className="col">
+            <TextField
+              label="Customer Name"
+              className="border-success"
               {...register("customerName", {
                 required: "Customer name is required.",
               })}
+              error={errors.customerName}
             />
-            <label>Customer Name</label>
           </div>
         </div>
         <div className="col">
-          <div className="form-floating mb-3">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Email"
-              {...register("Email", {
-                pattern: {
-                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                  message: "Incorrect email format.",
+          <TextField
+            label="Email"
+            className="border-success"
+            {...register("Email", {
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: "Incorrect email format.",
+              },
+              validate: {
+                notFake: (value) => {
+                  return value != "email@gmail.com" || "This email is blocked.";
                 },
-                validate: {
-                  notFake: (value) => {
-                    return (
-                      value != "email@gmail.com" || "This email is blocked."
-                    );
-                  },
-                  notFromBlackLostedDomain: (_, values) => {
-                    return (
-                      values.customerName == "admin" || "This form is required"
-                    );
-                  },
+                notFromBlackLostedDomain: (_, values) => {
+                  return (
+                    values.customerName == "admin" || "This form is required"
+                  );
                 },
-              })}
-            />
-            <label>Email</label>
-            {errors.Email && (
-              <div className="error-feedback">{errors.Email?.message}</div>
-            )}
-          </div>
+              },
+            })}
+            error={errors.Email}
+          />
         </div>
       </div>
 
